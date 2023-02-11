d611f42a7849b535ea7643f77521f9ecb1218e8d
Cleopatrakadzinga <118051305+Cleopatrakadzinga@users.noreply.github.com>
2023-02-11 12:15:22 +0200

    Update getinfo.c

--- getinfo.c
+++ getinfo.c
@@ -75,37 +75,19 @@ int _setenv(info_t *info, char *var, char *value)
 	_strcat(buf, value);
 	node = info->env;
 	while (node)
-
-										if (!info->cmd_buf)
-
-														free(info->arg);
-
-												if (info->env)
-
-																free_list(&(info->env));
-
-														if (info->history)
-
-																		free_list(&(info->history));
-
-																if (info->alias)
-
-																				free_list(&(info->alias));
-
-																		ffree(info->environ);
-
-																					info->environ = NULL;
-
-																							bfree((void **)info->cmd_buf);
-
-																									if (info->readfd > 2)
-
-																													close(info->readfd);
-
-																											_putchar(BUF_FLUSH);
-
-																												}
-
+	{
+		p = starts_with(node->str, var);
+		if (p && *p == '=')
+		{
+			free(node->str);
+			node->str = buf;
+			info->env_changed = 1;
+			return (0);
+		}
+		node = node->next;
+	}
+	add_node_end(&(info->env), buf, 0);
+	free(buf);
+	info->env_changed = 1;
+	return (0);
 }
-
-
