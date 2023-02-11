bd9562a503d4d3fc1998a7338df0472372f19402
Cleopatrakadzinga <118051305+Cleopatrakadzinga@users.noreply.github.com>
2023-02-11 12:00:50 +0200

    Update getenv.c

--- getenv.c
+++ getenv.c
@@ -1,20 +1,13 @@
-nclude "shell.h"
-
-
+#include "shell.h"
 
 /**
- *
- *  * get_environ - returns the string array copy of our environ
- *
- *   * @info: Structure containing potential arguments. Used to maintain
- *
- *    *          constant function prototype.
- *
- *     * Return: Always 0
- *
- *      */
-
+ * get_environ - returns the string array copy of our environ
+ * @info: Structure containing potential arguments. Used to maintain
+ *          constant function prototype.
+ * Return: Always 0
+ */
 char **get_environ(info_t *info)
+
 
 {
 
