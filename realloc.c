08a68aa0bca238b7defbc460a226934a03291462
Cleopatrakadzinga <118051305+Cleopatrakadzinga@users.noreply.github.com>
2023-02-11 12:52:11 +0200

    Update realloc.c

--- realloc.c
+++ realloc.c
@@ -16,74 +16,39 @@ char *_memset(char *s, char b, unsigned int n)
 	return (s);
 }
 
-
-
-
 /**
- *
- *  * ffree - frees a string of strings
- *
- *   * @pp: string of strings
- *
- *    */
-
+ * ffree - frees a string of strings
+ * @pp: string of strings
+ */
 void ffree(char **pp)
-
 {
+	char **a = pp;
 
-		char **a = pp;
-
-
-
-			if (!pp)
-
-						return;
-
-				while (*pp)
-
-							free(*pp++);
-
-					free(a);
-
+	if (!pp)
+		return;
+	while (*pp)
+		free(*pp++);
+	free(a);
 }
 
-
-
 /**
+ * _realloc - reallocates a block of memory
+ * @ptr: pointer to previous malloc'ated block
+ * @old_size: byte size of previous block
+ * @new_size: byte size of new block
  *
- *  * _realloc - reallocates a block of memory
- *
- *   * @ptr: pointer to previous malloc'ated block
- *
- *    * @old_size: byte size of previous block
- *
- *     * @new_size: byte size of new block
- *
- *      *
- *
- *       * Return: pointer to da ol'block nameen.
- *
- *        */
-
+ * Return: pointer to da ol'block nameen.
+ */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
-
 {
+	char *p;
 
-		char *p;
-
-
-
-			if (!ptr)
-
-						return (malloc(new_size));
-
-				if (!new_size)
-
-							return (free(ptr), NULL);
-
-					if (new_size == old_size)
-
-								return (ptr);
+	if (!ptr)
+		return (malloc(new_size));
+	if (!new_size)
+		return (free(ptr), NULL);
+	if (new_size == old_size)
+		return (ptr);
 
 
 
