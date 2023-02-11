fc7e796d74f88de4f3cd122d912406aaf13b7d0f
Cleopatrakadzinga <118051305+Cleopatrakadzinga@users.noreply.github.com>
2023-02-11 12:42:14 +0200

    Update main.c

--- main.c
+++ main.c
@@ -35,51 +35,10 @@ int main(int ac, char **av)
 			}
 			return (EXIT_FAILURE);
 		}
-
-										fd = open(av[1], O_RDONLY);
-
-												if (fd == -1)
-
-															{
-
-																			if (errno == EACCES)
-
-																								exit(126);
-
-																						if (errno == ENOENT)
-
-																										{
-
-																															_eputs(av[0]);
-
-																																			_eputs(": 0: Can't open ");
-
-																																							_eputs(av[1]);
-
-																																											_eputchar('\n');
-
-																																															_eputchar(BUF_FLUSH);
-
-																																																			exit(127);
-
-																																																						}
-
-																									return (EXIT_FAILURE);
-
-																											}
-
-														info->readfd = fd;
-
-															}
-
-						populate_env_list(info);
-
-							read_history(info);
-
-								hsh(info, av);
-
-									return (EXIT_SUCCESS);
-
+		info->readfd = fd;
+	}
+	populate_env_list(info);
+	read_history(info);
+	hsh(info, av);
+	return (EXIT_SUCCESS);
 }
-
-
