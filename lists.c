a16026827927029b6fbf1a20b852934c354cfbbe
Cleopatrakadzinga <118051305+Cleopatrakadzinga@users.noreply.github.com>
2023-02-11 12:32:20 +0200

    Update lists.c

--- lists.c
+++ lists.c
@@ -94,126 +94,53 @@ size_t print_list_str(const list_t *h)
 	return (i);
 }
 
-
-
-
 /**
+ * delete_node_at_index - deletes node at given index
+ * @head: address of pointer to first node
+ * @index: index of node to delete
  *
- *  * print_list_str - prints only the str element of a list_t linked list
- *
- *   * @h: pointer to first node
- *
- *    *
- *
- *     * Return: size of list
- *
- *      */
-
-size_t print_list_str(const list_t *h)
-
-{
-
-		size_t i = 0;
-
-
-
-			while (h)
-
-					{
-
-								_puts(h->str ? h->str : "(nil)");
-
-										_puts("\n");
-
-												h = h->next;
-
-														i++;
-
-															}
-
-				return (i);
-
-}
-
-
-
-/**
- *
- *  * delete_node_at_index - deletes node at given index
- *
- *   * @head: address of pointer to first node
- *
- *    * @index: index of node to delete
- *
- *     *
- *
- *      * Return: 1 on success, 0 on failure
- *
- *       */
-
+ * Return: 1 on success, 0 on failure
+ */
 int delete_node_at_index(list_t **head, unsigned int index)
-
 {
+	list_t *node, *prev_node;
+	unsigned int i = 0;
 
-		list_t *node, *prev_node;
+	if (!head || !*head)
+		return (0);
 
-			unsigned int i = 0;
-
-
-
-				if (!head || !*head)
-
-							return (0);
-
-
-
-					if (!index)
-
-							{
-
-										node = *head;
-
-												*head = (*head)->next;
-
-														free(node->str);
-
-																free(node);
-
-																		return (1);
-
-																			}
-
-						node = *head;
-
-							while (node)
-
-									{
-
-												if (i == index)
-
-															{
-
-																			prev_node->next = node->next;
-
-																						free(node->str);
-
-																									free(node);
-
-																												return (1);
-
-																														}
-
-														i++;
-
-																prev_node = node;
-
-																		node = node->next;
-
-																			}
-
-								return (0);
-
+	if (!index)
+	{
+		node = *head;
+		*head = (*head)->next;
+		free(node->str);
+		free(node);
+		return (1);
+	}
+	node = *head;
+	while (node)
+	{
+		if (i == index)
+		{
+			prev_node->next = node->next;
+			free(node->str);
+			free(node);
+			return (1);
+		}
+		i++;
+		prev_node = node;
+		node = node->next;
+	}
+	return (0);
 }
+
+/**
+ * free_list - frees all nodes of a list
+ * @head_ptr: address of pointer to head node
+ *
+ * Return: void
+ */
+
 
 
 
