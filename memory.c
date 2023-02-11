fa0b40173d6eb34f65c05d10ef297f9b2b5cf4e1
Cleopatrakadzinga <118051305+Cleopatrakadzinga@users.noreply.github.com>
2023-02-11 12:44:49 +0200

    Update memory.c

--- memory.c
+++ memory.c
@@ -4,19 +4,12 @@
  * bfree - frees a pointer and NULLs the address
  * @ptr: address of the pointer to free
  *
-
-
+ * Return: 1 if freed, otherwise 0.
+ */
 int bfree(void **ptr)
-
 {
+	if (ptr && *ptr)
 
-		if (ptr && *ptr)
-
-				{
-
-							free(*ptr);
-
-									*ptr = NULL;
 
 											return (1);
 
