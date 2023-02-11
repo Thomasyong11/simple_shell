936af033c841105b37e136aedb74b5a1aca5e485
Cleopatrakadzinga <118051305+Cleopatrakadzinga@users.noreply.github.com>
2023-02-11 13:07:43 +0200

    Update string.c

--- string.c
+++ string.c
@@ -66,73 +66,10 @@ char *_strcat(char *dest, char *src)
 {
 	char *ret = dest;
 
-
-
-
-/**
- *
- *  * starts_with - checks if needle starts with haystack
- *
- *   * @haystack: string to search
- *
- *    * @needle: the substring to find
- *
- *     *
- *
- *      * Return: address of next char of haystack or NULL
- *
- *       */
-
-char *starts_with(const char *haystack, const char *needle)
-
-{
-
-		while (*needle)
-
-					if (*needle++ != *haystack++)
-
-									return (NULL);
-
-			return ((char *)haystack);
-
+	while (*dest)
+		dest++;
+	while (*src)
+		*dest++ = *src++;
+	*dest = *src;
+	return (ret);
 }
-
-
-
-/**
- *
- *  * _strcat - concatenates two strings
- *
- *   * @dest: the destination buffer
- *
- *    * @src: the source buffer
- *
- *     *
- *
- *      * Return: pointer to destination buffer
- *
- *       */
-
-char *_strcat(char *dest, char *src)
-
-{
-
-		char *ret = dest;
-
-
-
-			while (*dest)
-
-						dest++;
-
-				while (*src)
-
-							*dest++ = *src++;
-
-					*dest = *src;
-
-						return (ret);
-
-}
-
-
