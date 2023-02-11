cfc8bc4871e3b5c8f31f72802228ffdc57de5620
Cleopatrakadzinga <118051305+Cleopatrakadzinga@users.noreply.github.com>
2023-02-11 13:10:16 +0200

    Update string1.c

--- string1.c
+++ string1.c
@@ -45,92 +45,37 @@ char *_strdup(const char *str)
 	return (ret);
 }
 
-
-}
-
-
-
 /**
+ * _puts - prints an input string
+ * @str: the string to be printed
  *
- *  * _strdup - duplicates a string
- *
- *   * @str: the string to duplicate
- *
- *    *
- *
- *     * Return: pointer to the duplicated string
- *
- *      */
-
-char *_strdup(const char *str)
-
-{
-
-		int length = 0;
-
-			char *ret;
-
-
-
-				if (str == NULL)
-
-							return (NULL);
-
-					while (*str++)
-
-								length++;
-
-						ret = malloc(sizeof(char) * (length + 1));
-
-							if (!ret)
-
-										return (NULL);
-
-								for (length++; length--;)
-
-											ret[length] = *--str;
-
-									return (ret);
-
-}
-
-
-
-/**
- *
- *  * _puts - prints an input string
- *
- *   * @str: the string to be printed
- *
- *    *
- *
- *     * Return: Nothing
- *
- *      */
-
+ * Return: Nothing
+ */
 void _puts(char *str)
-
 {
+	int i = 0;
 
-		int i = 0;
-
-
-
-			if (!str)
-
-						return;
-
-				while (str[i] != '\0')
-
-						{
-
-									_putchar(str[i]);
-
-											i++;
-
-												}
-
+	if (!str)
+		return;
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
 }
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	static int i;
+	static char buf[WRITE_BUF_SIZE];
+
 
 
 
