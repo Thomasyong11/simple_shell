29c4fd5aa3bbfa3a2d3b54dec5505b11bb63d328
Cleopatrakadzinga <118051305+Cleopatrakadzinga@users.noreply.github.com>
2023-02-11 12:01:55 +0200

    Update getenv.c

--- getenv.c
+++ getenv.c
@@ -7,23 +7,23 @@
  * Return: Always 0
  */
 char **get_environ(info_t *info)
-
-
 {
+	if (!info->environ || info->env_changed)
+	{
+		info->environ = list_to_strings(info->env);
+		info->env_changed = 0;
+	}
 
-		if (!info->environ || info->env_changed)
+	return (info->environ);
+}
 
-				{
-
-							info->environ = list_to_strings(info->env);
-
-									info->env_changed = 0;
-
-										}
-
-
-
-			return (info->environ);
+/**
+ * _unsetenv - Remove an environment variable
+ * @info: Structure containing potential arguments. Used to maintain
+ *        constant function prototype.
+ *  Return: 1 on delete, 0 otherwise
+ * @var: the string env var property
+ */
 
 }
 
